8fcfd75d50c9c64e0db87eb7b0b68bb70ef089f5
Jesse Giammattolo <giammattolo@hotmail.com>
2020-03-22 19:41:11 -0400

    v2 style fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from './components/Header'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
-// import Technologies from './components/Technologies';
 import Footer from './components/Footer'
 import About from './components/About'
 import './App.css';
@@ -14,7 +13,6 @@ function App() {
       <Header />
       <Projects />
       <About />
-      {/* <Technologies /> */}
       <Contact />
       <Footer />
     </div>
